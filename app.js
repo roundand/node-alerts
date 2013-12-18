f76041d50883f5ee1aa5292a19953ab5bb344b4c
Francis Norton <francis.norton@gmail.com>
2013-12-18 12:29:52 +0000

    added timer-triggered event

--- app.js
+++ app.js
@@ -7,25 +7,39 @@ function fred(message){
 
 var ee = new EventEmitter();
 ee.setMaxListeners(20);
-ee.once("someEvent", function () { console.log("event 1"); });
-ee.on("someEvent", function () { console.log("event 2"); });
-ee.once("someEvent", function () { console.log("event 3"); });
-ee.on("someEvent", function () { console.log("event 4"); });
-ee.once("someEvent", function () { console.log("event 5"); });
-ee.on("someEvent", function () { console.log("event 6"); });
-ee.once("someEvent", function () { console.log("event 7"); });
-ee.on("someEvent", function () { console.log("event 8"); });
-ee.once("someEvent", function () { console.log("event 9"); });
-ee.on("someEvent", function () { console.log("event 10"); });
-ee.once("someEvent", function () { console.log("event 11"); });
+
 ee.on("heyFred", fred);
-ee.emit("heyFred", "It's " + new Date().toISOString());
- 
-ee.emit("someEvent");
-ee.emit("someEvent");
 
-ee.emit("heyFred", "It's " + new Date().toISOString());
-ee.removeListener("heyFred", fred);
+// steady... wait for it...
+setTimeout(function(){ee.emit("heyFred", "Surprise - it's " + new Date().toISOString())}, 1);
+
+// do heyFred right now
 ee.emit("heyFred", "It's " + new Date().toISOString());
 
-console.log(util.inspect(ee.listeners('someEvent')));
+// now do the someEvent stuff
+ee.once("someEvent", function (msg) { console.log("event 1: " + msg); });
+ee.on("someEvent", function (msg) { console.log("event 2: " + msg); });
+ee.once("someEvent", function (msg) { console.log("event 3: " + msg); });
+ee.on("someEvent", function (msg) { console.log("event 4: " + msg); });
+ee.once("someEvent", function (msg) { console.log("event 5: " + msg); });
+ee.on("someEvent", function (msg) { console.log("event 6: " + msg); });
+ee.once("someEvent", function (msg) { console.log("event 7: " + msg); });
+ee.on("someEvent", function (msg) { console.log("event 8: " + msg); });
+ee.once("someEvent", function (msg) { console.log("event 9: " + msg); });
+ee.on("someEvent", function (msg) { console.log("event 10: " + msg); });
+ee.once("someEvent", function (msg) { console.log("event 11: " + msg); });
+
+// should trigger all registered listeners above 
+ee.emit("someEvent", "first time");
+
+// should trigger only the on() listeners, the once() listeners are released
+ee.emit("someEvent", "second time");
+
+// remove all remaining listeners
+ee.listeners('someEvent').forEach(function(element, index, array){ee.removeListener('someEvent', element)});
+
+// should should be no remaining listeners
+ee.emit("someEvent", "third time");
+
+// one more heyFred for luck
+ee.emit("heyFred", "It's " + new Date().toISOString());
